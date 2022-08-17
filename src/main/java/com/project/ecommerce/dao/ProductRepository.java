973191662e2435f21f2c4ce638a799546f945ad2
KevimLSDS <kevim.lhouis@outlook.com>
2022-08-17 20:31:27 -0300

    Creating repositories

---
+++ src/main/java/com/project/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.project.ecommerce.dao;
+
+import com.project.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+}
