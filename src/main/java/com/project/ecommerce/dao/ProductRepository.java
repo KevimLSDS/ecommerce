38487bd8f8e4754e39b2edcd5fb6b6e2aaf37ac8
KevimLSDS <kevim.lhouis@outlook.com>
2022-08-17 21:57:41 -0300

    Adding cross origin support

--- src/main/java/com/project/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/project/ecommerce/dao/ProductRepository.java
@@ -3,8 +3,10 @@ package com.project.ecommerce.dao;
 import com.project.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
