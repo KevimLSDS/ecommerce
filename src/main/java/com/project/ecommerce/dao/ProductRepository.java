278fe894441f5b5d2b76abdc782827d3a47c4af5
KevimLSDS <kevim.lhouis@outlook.com>
2022-08-22 20:40:38 -0300

    Adding custom search on repository

--- src/main/java/com/project/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/project/ecommerce/dao/ProductRepository.java
@@ -1,7 +1,12 @@
 package com.project.ecommerce.dao;
 
 import com.project.ecommerce.entity.Product;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -9,5 +14,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
